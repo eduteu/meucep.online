29dc05484fa55d317032636eb8b13068e5850cc5
Eduardo Palandrani <edupalandrani@gmail.com>
2018-08-28 23:07:04 -0300

    Add files via upload

---
+++ exemplo.php
@@ -0,0 +1,18 @@
+<?php 
+
+# Script de exemplo para funcionamento do webservice do site app.meucep.online
+
+
+/*************************/
+# Autor: Eduardo Palandrani
+# Data e Hora: 2018-08-28 23:05:27
+/*************************/
+
+function BuscaCEP($cep){ # Função simplificada do exemplo para busca de CEP no meucep.online
+	$retorno = file_get_contents('http://app.meucep.online?KEY=SUACHAVE&CEP=01001-000');
+	
+	echo $retorno; # Apague essa linha para não mostrar mais na tela os dados em JSON
+
+	# Basta comentar a linha de baixo para tratar os dados que vem em JSON
+	//$cep = json_decode($retorno); 
+}
